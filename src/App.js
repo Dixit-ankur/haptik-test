0c8595ef9e787138918efb279029fd017c9a0c07
Ankur Dixit <ankur.dixit@acuverconsulting.com>
2021-06-03 12:04:58 +0530

    "haptik-test code"

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FriendList from './component/FriendList';
+
+function App() {
+  return (
+    <div className="App">
+      <FriendList />
+    </div>
+  );
+}
+
+export default App;
